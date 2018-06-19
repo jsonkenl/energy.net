4b67325cdcbafd5cd9fbc5fdc3e8479752ce3283
Jason Kennell <jsonkenl@gmail.com>
2018-06-19 14:47:28 -0600

    Initial commit
    - Created .gitignore file
    - Implemented feature folder functionality:
            The web application project will be organized by features. The
            controller, viewmodels and views for a particular feature will
            be contained within a single folder for that feature
    - Added bower for package management:
            Set up bower, required files and installed bootswatch, jquery
            and font-awesome

---
+++ Energy.Net/FeaturesViewLocationExpander.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.Razor;
+using System.Collections.Generic;
+
+namespace Energy.Net
+{
+    public class FeaturesViewLocationExpander : IViewLocationExpander
+    {
+        public void PopulateValues(ViewLocationExpanderContext context)
+        {
+            context.Values["customviewlocation"] = nameof(FeaturesViewLocationExpander);
+        }
+
+        public IEnumerable<string> ExpandViewLocations(ViewLocationExpanderContext context, IEnumerable<string> viewLocations)
+        {
+            var controllerActionDescriptor = context.ActionContext.ActionDescriptor as ControllerActionDescriptor;
+            string featureName = controllerActionDescriptor.Properties["feature"] as string;
+            foreach (var location in viewLocations)
+            {
+                yield return location.Replace("{3}", featureName);
+            }
+        }
+    }
+}
---
+++ Energy.Net/Startup.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Energy.Net
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // MVC with Feature Folders
+            services.AddMvc(o => o.Conventions.Add(new FeatureControllerConvention()))
+                .AddRazorOptions(options =>
+                {
+                    // {0} - Action Name
+                    // {1} - Controller Name
+                    // {2} - Area Name
+                    // {3} - Feature Name
+                    // Replace normal view location entirely
+                    options.ViewLocationFormats.Clear();
+                    options.ViewLocationFormats.Add("/Features/{3}/{1}/{0}.cshtml");
+                    options.ViewLocationFormats.Add("/Features/{3}/{0}.cshtml");
+                    options.ViewLocationFormats.Add("/Features/Shared/{0}.cshtml");
+                    options.ViewLocationExpanders.Add(new FeaturesViewLocationExpander());
+                });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
